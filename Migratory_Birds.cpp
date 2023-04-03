d8a3062e27b6eaa54bd1dc6ed6e0e0a092e72ade
Vaibhav T Gaikwad <103184302+VaibhavGaikwad03@users.noreply.github.com>
2023-04-03 15:47:01 +0530

    Add files via upload

---
+++ Migratory_Birds.cpp
@@ -0,0 +1,105 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string ltrim(const string &);
+string rtrim(const string &);
+vector<string> split(const string &);
+
+/*
+ * Complete the 'migratoryBirds' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+ 
+int migratoryBirds(std::vector<int> arr) 
+{
+    int freq[6] = {0};  // Initialize frequency array with 0's
+    
+    for (auto birdType : arr) 
+    {
+        freq[birdType]++;  // Increment frequency of bird type
+    }
+    
+    int maxFreq = 0, maxBirdType = 0;
+    
+    for (int i = 1; i <= 5; ++i) 
+    {  // Check each bird type from 1 to 5
+        if (freq[i] > maxFreq) 
+        {
+            maxFreq = freq[i];  // Update max frequency
+            maxBirdType = i;  // Update bird type with max frequency
+        }
+    }
+    return maxBirdType;
+}
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string arr_count_temp;
+    getline(cin, arr_count_temp);
+
+    int arr_count = stoi(ltrim(rtrim(arr_count_temp)));
+
+    string arr_temp_temp;
+    getline(cin, arr_temp_temp);
+
+    vector<string> arr_temp = split(rtrim(arr_temp_temp));
+
+    vector<int> arr(arr_count);
+
+    for (int i = 0; i < arr_count; i++) {
+        int arr_item = stoi(arr_temp[i]);
+
+        arr[i] = arr_item;
+    }
+
+    int result = migratoryBirds(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
+
+string ltrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        s.begin(),
+        find_if(s.begin(), s.end(), not1(ptr_fun<int, int>(isspace)))
+    );
+
+    return s;
+}
+
+string rtrim(const string &str) {
+    string s(str);
+
+    s.erase(
+        find_if(s.rbegin(), s.rend(), not1(ptr_fun<int, int>(isspace))).base(),
+        s.end()
+    );
+
+    return s;
+}
+
+vector<string> split(const string &str) {
+    vector<string> tokens;
+
+    string::size_type start = 0;
+    string::size_type end = 0;
+
+    while ((end = str.find(" ", start)) != string::npos) {
+        tokens.push_back(str.substr(start, end - start));
+
+        start = end + 1;
+    }
+
+    tokens.push_back(str.substr(start));
+
+    return tokens;
+}
